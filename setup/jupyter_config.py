5c3ff83da0bb147a2a7bc49cbc213053bd17f0eb
Tenzo-0 <hanhatbao352008@gmail.com>
2024-10-03 09:48:54 +0700

    Update jupyter_config.py

--- setup/jupyter_config.py
+++ setup/jupyter_config.py
@@ -6,4 +6,4 @@ c.ServerApp.allow_remote_access = True
 c.ServerApp.allow_origin = '*'
 c.ServerApp.root_dir = "/home/user"
 
-PasswordIdentityProvider.hashed_password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$Z3Fb2goTsoEu7JoaNqxzfA$QI+UWp2+4Mae8xW85EZ2p2+cI4emAGbOZroQy/SmyKE'
+PasswordIdentityProvider.hashed_password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$Ct7uQIdNDrPp6LHrGPfANQ$jq/RQiaN7fLxa4biy3G87Dz26DRRzM0ZEf0xqGuKkdI'
